19898744b9764ae23e5905974b8990c164032158
Pirjo I Majander <pimajand@melkki.cs.helsinki.fi>
2012-05-23 17:37:05 +0300

    Reseptikortistoa ja Reseptiä muokattu

--- Reseptikortisto/src/reseptikortisto/Resepti.java
+++ Reseptikortisto/src/reseptikortisto/Resepti.java
@@ -4,10 +4,38 @@
  */
 package reseptikortisto;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author pimajand
  */
 public class Resepti {
+    private String resepti;
+    private String sijainti;
+    private ArrayList<String> hakukriteerit = new ArrayList<String>();
+        
+   public Resepti(String nimi, String sijainti) {
+        this.resepti = nimi;
+        this.sijainti = sijainti;
+               
+   }
+   
+   public void muutaNimea(Resepti resepti, String uusiNimi) {
+       this.resepti = uusiNimi;
+       return;
+   }
+     
+   public void muutaSijaintia(Resepti resepti, String uusiSijainti) {
+       this.sijainti = uusiSijainti;
+       return;
+   }
+   
+    @Override
+public String toString() {
+    return this.resepti + ", " + this.sijainti + ", hakukriteeri puuttuu vielä";
+}       
+  
     
 }
--- Reseptikortisto/src/reseptikortisto/Reseptikortisto.java
+++ Reseptikortisto/src/reseptikortisto/Reseptikortisto.java
@@ -4,16 +4,26 @@
  */
 package reseptikortisto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pimajand
  */
 public class Reseptikortisto {
+    private static Scanner lukija = new Scanner(System.in);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Resepti resepti = new Resepti("Tonnikalalasagne", "Ruutuvihko");
+        System.out.println(resepti);
+        
+        
+        
+        
+        
         // TODO code application logic here
     }
 }
